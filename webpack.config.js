6098c98bc8ba46f702781b123a71c000d7db2192
宁培希 <1329467963@qq.com>
2020-06-02 23:02:43 +0800

    performance配置

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,24 @@ module.exports ={
             })
         ]
     },
+    
+performance: {
+ 
+    hints: "warning", // 枚举
+     
+    maxAssetSize: 30000000, // 整数类型（以字节为单位）
+     
+    maxEntrypointSize: 50000000, // 整数类型（以字节为单位）
+     
+    assetFilter: function(assetFilename) {
+     
+    // 提供资源文件名的断言函数
+     
+    return assetFilename.endsWith('.css') || assetFilename.endsWith('.js');
+     
+    }
+     
+    },
     module: { // 配置所有第三方loader 模块的
         rules: [ // 第三方模块的匹配规则
           { test: /\.css$/, use: ['style-loader', 'css-loader'] }, // 处理 CSS 文件的 loader
